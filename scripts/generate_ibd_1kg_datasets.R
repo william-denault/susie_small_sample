49fc32099b9a96b73c76a6a383a0dea0de8f28ad
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-09-03 16:21:13 -0500

    Added omni_samples.20141118.panel; converted hg19 coordinates in ibd_finemap_huang2017.csv to hg38.

--- scripts/generate_ibd_1kg_datasets.R
+++ scripts/generate_ibd_1kg_datasets.R
@@ -3,7 +3,4 @@
 # https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20181203_biallelic_SNV/
 # https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/hd_genotype_chip/omni_samples.20141118.panel
 #
-# Huang et al. Fine-mapping inflammatory bowel disease loci to
-# single-variant resolution. Nature 547, 173–178 (2017).
-# https://doi.org/10.1038/nature22969
-# Supplementary Table 1.
+regions <- read.csv()

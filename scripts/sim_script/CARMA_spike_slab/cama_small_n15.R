efc59c0647d8495568836d8549da96e61a7d6066
WD <william-denault@users.noreply.github.com>
2024-10-21 14:53:55 +0200

    update

---
+++ scripts/sim_script/CARMA_spike_slab/cama_small_n15.R
@@ -0,0 +1,32 @@
+library(susieR
+)
+
+rm(list=ls())
+source("/home/wdenault/susie_small_sample/scripts/sim_script/CARMA_sample_sim_spike_slab.R")
+
+for ( o in 1:20000){
+
+
+  temp0 <- run_CARMA_sim(N=50, h=0.75, n_sim=100)
+
+  if(file.exists("/home/wdenault/susie_small_sample/simulations/small_sample_CARMA_spike_slab50_h75.RData")){
+    load("/home/wdenault/susie_small_sample/simulations/small_sample_CARMA_spike_slab50_h75.RData")
+    if(!is.null(temp)){
+      temp <- rbind (temp, temp0)
+    }else{
+      temp <- temp0
+    }
+
+
+
+  }else{
+    temp <- temp0
+  }
+
+  save(temp, file="/home/wdenault/susie_small_sample/simulations/small_sample_CARMA_spike_slab50_h75.RData")
+
+
+
+
+
+}

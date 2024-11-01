eb4076603f6ac7f9880c1fdcd4f425bfb795de28
WD <william.denault@gmail.com>
2024-11-01 14:44:05 +0100

    a

--- scripts/sim_script/L_1_SuSiE/susie_small_n22.R
+++ scripts/sim_script/L_1_SuSiE/susie_small_n22.R
@@ -5,7 +5,7 @@ rm(list=ls())
 source("/home/wdenault/susie_small_sample/scripts/sim_script/small_sample_sims.R")
 for ( o in 1:10000){
 
-  temp0 <- run_susie_sim(N=100, h=0.50, n_sim=1000, L_sim = 1, L_susie = 1)
+  temp0 <- run_susie_sim(N=100, h=0.50, n_sim=100, L_sim = 1, L_susie = 1)
 
   if(file.exists("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie100_h50.RData")){
     load("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie100_h50.RData")

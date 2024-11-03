374ba552df546609224454cbbb4011238338758b
WD <william.denault@gmail.com>
2024-11-03 21:11:16 +0100

    some more script and correction

--- scripts/sim_script/L_1_SuSiE/susie_small_n5.R
+++ scripts/sim_script/L_1_SuSiE/susie_small_n5.R
@@ -4,7 +4,7 @@ library(susieR
 rm(list=ls())
 source("/home/wdenault/susie_small_sample/scripts/sim_script/small_sample_sims.R")
 for ( o in 1:10000){
-  temp0 <- run_susie_sim(N=75, h=0.25, n_sim=1000, L_sim = 1, L_susie = 1)
+  temp0 <- run_susie_sim(N=75, h=0.25, n_sim=100, L_sim = 1, L_susie = 1)
 
   if(file.exists("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h25.RData")){
     load("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h25.RData")
@@ -19,57 +19,6 @@ for ( o in 1:10000){
 
   save(temp, file="/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h25.RData")
 
-
-
-
-  temp0 <- run_susie_sim(N=75, h=0.50, n_sim=1000 , L_sim = 1, L_susie = 1)
-
-  if(file.exists("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h50.RData")){
-    load("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h50.RData")
-    if(!is.null(temp)){
-      temp <- rbind (temp, temp0)
-    }else{
-      temp <- temp0
-    }
-  }else{
-    temp <- temp0
-  }
-
-  save(temp, file="/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h50.RData")
-
-
-
-  temp0 <- run_susie_sim(N=75, h=0.30, n_sim=1000, L_sim = 1, L_susie = 1)
-
-
-  if(file.exists("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h30.RData")){
-    load("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h30.RData")
-    if(!is.null(temp)){
-      temp <- rbind (temp, temp0)
-    }else{
-      temp <- temp0
-    }
-  }else{
-    temp <- temp0
-  }
-
-  save(temp, file="/home/wdenault/susie_small_sample/simulations/small_sample_susie75_h30.RData")
-
-
-  temp0 <- run_susie_sim(N=75, h=0.75, n_sim=1000, L_sim = 1, L_susie = 1)
-
-  if(file.exists("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h75.RData")){
-    load("/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h75.RData")
-    if(!is.null(temp)){
-      temp <- rbind (temp, temp0)
-    }else{
-      temp <- temp0
-    }
-  }else{
-    temp <- temp0
-  }
-
-  save(temp, file="/home/wdenault/susie_small_sample/simulations/L_1_small_sample_susie75_h75.RData")
 
 
 }

340e53a7c80683bca8a9d6513c05531dabe69003
WD <william.denault@gmail.com>
2024-12-03 23:05:44 +0100

    adding ideal sim

---
+++ scripts/sim_script/ideal_sim/n20.R
@@ -0,0 +1,18 @@
+source("C:/Document/Serieux/Travail/Package/susie_small_sample/scripts/sim_script/ideal_susie_sim.R" )
+res=  run_susie_sim (N=20,
+                     sd_res = 1,
+                     sd_prior = 0.5)
+
+
+
+
+for ( i in 1:2000){
+
+  tres =run_susie_sim (N=20,
+                       sd_res = 1,
+                       sd_prior = 0.5)
+
+  res= rbind(tres,res)
+  save( res, file="C:/Document/Serieux/Travail/Package/susie_small_sample/local_sim/n20.RData")
+
+}

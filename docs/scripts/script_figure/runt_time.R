0f69cff56064984f2b3618be60ff6ad404d9532d
WD <william-denault@users.noreply.github.com>
2025-02-07 14:47:24 +0100

    continue refomatting repo

--- docs/scripts/script_figure/runt_time.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-
-load("~/susie_small_sample/simulations/run_time.RData")
-ggplot(res, aes(x=Method, y=time))+
-  geom_boxplot()+
-  scale_y_log10()+
- # facet_wrap(.~N)+
-  ggtitle("Comparison of computation time (seconds)")

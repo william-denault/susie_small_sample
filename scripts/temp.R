ed4c05efaaaa9cde754dd56b1985f23e6eb1dd4d
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-03-09 15:20:11 -0500

    Added temp.R script to better understand these latest results.

--- scripts/temp.R
+++ scripts/temp.R
@@ -8,8 +8,19 @@ j <- which(x == 1)
 lbf0 <- sapply(res_susie[i],function (x) x$lbf)
 lbf1 <- sapply(res_susie[j],function (x) x$lbf)
 pdat1 <- rbind(data.frame(causal = FALSE,lbf = lbf0),
-              data.frame(causal = TRUE,lbf = lbf1))
+               data.frame(causal = TRUE,lbf = lbf1))
+
+lbf0 <- sapply(res_susie_small[i],function (x) x$lbf)
+lbf1 <- sapply(res_susie_small[j],function (x) x$lbf)
+pdat2 <- rbind(data.frame(causal = FALSE,lbf = lbf0),
+               data.frame(causal = TRUE,lbf = lbf1))
+
+pdat <- data.frame(causal      = pdat1$causal,
+                   susie       = pdat1$lbf,
+                   susie_small = pdat2$lbf)
+
 pdat1 <- transform(pdat1,lbf = log10(pmax(0,lbf)))
+pdat2 <- transform(pdat2,lbf = log10(pmax(0,lbf)))
 p1 <- ggplot(pdat1,aes(x = lbf,color = causal,fill = causal)) +
   geom_histogram(position = "stack") +
   scale_color_manual(values = c("darkblue","red")) +
@@ -17,12 +28,6 @@ p1 <- ggplot(pdat1,aes(x = lbf,color = causal,fill = causal)) +
   labs(x = "log10 LBF",title = "susie") +
   xlim(c(-4,2)) +
   theme_cowplot(font_size = 12)
-
-lbf0 <- sapply(res_susie_small[i],function (x) x$lbf)
-lbf1 <- sapply(res_susie_small[j],function (x) x$lbf)
-pdat2 <- rbind(data.frame(causal = FALSE,lbf = lbf0),
-              data.frame(causal = TRUE,lbf = lbf1))
-pdat2 <- transform(pdat2,lbf = log10(pmax(0,lbf)))
 p2 <- ggplot(pdat2,aes(x = lbf,color = causal,fill = causal)) +
   geom_histogram(position = "stack") +
   scale_color_manual(values = c("darkblue","red")) +
@@ -30,5 +35,14 @@ p2 <- ggplot(pdat2,aes(x = lbf,color = causal,fill = causal)) +
   labs(x = "log10 LBF",title = "susie_small") +
   xlim(c(-4,2)) +
   theme_cowplot(font_size = 12)
+plot_grid(p1,p2,nrow = 2,ncol = 1)
 
-print(plot_grid(p1,p2,nrow = 2,ncol = 1))
+n <- nrow(pdat)
+p3 <- ggplot(pdat[n:1,],aes(x = susie,y = susie_small,color = causal)) +
+  geom_point() +
+  geom_abline(intercept = 0,slope = 1,color = "magenta",
+              linetype = "dotted") +
+  scale_color_manual(values = c("darkblue","red")) +
+  xlim(-1,90) +
+  ylim(-1,90) +
+  theme_cowplot(font_size = 12)
